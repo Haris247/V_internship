b30508d2515f1cea86bf012223a7582bc587bc11
haris <haris.akram764@gmail.com>
2024-06-21 16:45:04 +0500

    adding command file

---
+++ Commands.sh
@@ -0,0 +1,13 @@
+git git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Haris247/hello.git
+git push -u origin main
+git branch
+git branch update_logo
+git checkout update_logo
+git merge update_log
+git add .
+git status
+git push 
